a67f9633295a55b6a8f367796f8659bb949331c0
RunninglVlan <RedCapShortCut@gmail.com>
2022-04-03 23:18:27 +0300

    fix: end-turn is disabled during water raise

--- Assets/Scripts/MainUI.cs
+++ Assets/Scripts/MainUI.cs
@@ -2,14 +2,20 @@ using UnityEngine.UIElements;
 
 public class MainUI : UI {
     private Label resources = null!;
+    private Button endTurn = null!;
 
     protected override void Awake() {
         base.Awake();
         resources = documentRoot.Q<Label>("resources");
         FindObjectOfType<ResourceManager>().ResourcesUpdated += SetResources;
         var game = FindObjectOfType<Game>();
-        documentRoot.Q<Button>("end-turn").clicked += game.EndTurn;
+        game.TurnEnded += DisableEndTurn;
+        endTurn = documentRoot.Q<Button>("end-turn");
+        endTurn.clicked += game.EndTurn;
+        FindObjectOfType<Water>().Raised += EnableEndTurn;
     }
 
     private void SetResources(int count) => resources.text = count.ToString();
+    private void DisableEndTurn() => endTurn.SetEnabled(false);
+    private void EnableEndTurn(float _) => endTurn.SetEnabled(true);
 }
